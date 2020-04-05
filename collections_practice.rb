aaa29957bc44e5399f8bc13899d5939b74a791bf
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-05 08:34:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,3 +14,11 @@ def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   return array
 end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each {|word| word[2] = "$"}
+end
