a7b4015582209ebd93f3369ee7b8bd5f7da7820b
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-05 08:31:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,9 +11,5 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  x = 0
-  array[1] = x
-  array[1] = array[2]
-  array[2] = x
-  return array
+  array[1], array[2] = array[2], array[1]
 end
